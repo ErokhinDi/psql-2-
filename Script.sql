e141551df471efe024b475dc11d9399c1fb7ef9c
ErokhinDi <86659688+ErokhinDi@users.noreply.github.com>
2022-06-15 21:40:48 +1000

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTs singer(
+id SERIAL primary key ,
+name VARCHAR(20) not null,
+album VARCHAR(30) not null,
+id_style_song INTEGER references style_song(id) not null 
+);
+
+CREATE TABLE IF NOT EXISTS style_song(
+id SERIAL PRIMARY KEY ,
+name VARCHAR(20) NOT null
+
+);
+
+CREATE table IF NOT EXISTS album_name (
+id SERIAL PRIMARY KEY,
+name VARCHAR(20) NOT NULL,
+date INTEGER NOT null,
+id_singer INTEGER references singer(id) not null
+);
+
+create table if not exists track(
+id SERIAL primary key references id_album_name,
+name varchar(30) not null,
+track_name_id varchar(30) not null
+);
+create table if not exists collections(
+id SERIAL references track_name_id not null,
+name Varchar(30) ,
+date_release INTEGER not null,
+id_track VARCHAR(40)not NULL
+);
+create table lim(
+id_singer INTEGER REFERENCES singer(id),
+id_album INTEGER REFERENCES album_name(id),
+constraint pk primary key (id_singer, id_album)
+);
